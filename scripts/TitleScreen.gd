4ff6fae8014880bb39af7647dee37235185317ef
Gwompaholic <116659416+gwompy@users.noreply.github.com>
2025-04-08 21:02:47 -0400

    Update TitleScreen.gd
    the Play button now uses a fade transition.

--- scripts/TitleScreen.gd
+++ scripts/TitleScreen.gd
@@ -6,4 +6,4 @@ func _on_control_button_pressed() -> void:
 
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/world.tscn")
+	TransitionScreen.transition("res://scenes/world.tscn")
