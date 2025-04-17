dd8f8ffa9ca93b6a2d89b6d28f02c7595b21f7c6
Gwompaholic <116659416+gwompy@users.noreply.github.com>
2025-04-16 21:29:57 -0400

    clean slate
    too many updates, corruption is rampant.

--- scripts/TitleScreen.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-
-func _on_control_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/control_page.tscn")
-
-
-func _on_play_button_pressed() -> void:
-	TransitionScreen.transition("res://scenes/world.tscn")
